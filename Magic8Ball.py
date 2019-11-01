3f68be7e3dd6207f52002a6c5f467d0f835668fc
azmathias <azramathias@stuperuser.onmicrosoft.com>
2019-11-01 13:21:08 +0000

    Add Magic 8 Ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,10 +1,10 @@
 # Authour: 	azmathias Stuperuser Ltd
 # Purpose: Magic 8 Ball
-#	Simulate a magic 8-ball.
-#	Allow the user to enter their question.
-#	Display an in progress message(i.e. "thinking").
-#	Create 20 responses, and show a random response.
-#	Allow the user to ask another question or quit.
+#			Simulate a magic 8-ball.
+#			Allow the user to enter their question.
+#			Display an in progress message(i.e. "thinking").
+#			Create 20 responses, and show a random response.
+#			Allow the user to ask another question or quit.
 # Bonus:
 #	Add a gui.
 #	It must have box for users to enter the question.
@@ -13,4 +13,46 @@
 #	clear (the text box)
 #	play again
 #	quit (this must close the window)
+# TODO: finish GUI
+
+
+import random
+import time
+
+print("Welcome to the Magic 8 Ball!")
+
+responses = ["Yes, most definitely!"
+			, "The chances are high!"
+			, "Not likely!"
+			, "May the odds be in your favour."
+			, "No chance kiddo!"
+			, "99.9% success rate!"
+			, "Congratulations, yes!"
+			, "Ask again later!"
+			, "Highly unlikely"
+			, "Concentrate and ask again"
+			]
+
+def userquestion():
+	question = input("\nYou may ask a question.\n")
+	print("\nThinking...\n")
+	time.sleep(random.randrange(0,5))
+	print(random.choice(responses))
+	
+while True: 
+	userquestion()
+	repeat = input("Would you like to ask another question? (y/n)")
+	if not (repeat == "y" or repeat == "Y"):
+		print("\nThank you for playing!")
+		break
+		
+
+
+
+
+
+		
+
+	
+	
 
