f2d617a832dd39606fcedc3da64c2679d7af7931
azmathias <azramathias@stuperuser.onmicrosoft.com>
2019-11-01 11:24:48 +0000

    Add Pythag

---
+++ PythagoreanTriplesChecker.py
@@ -0,0 +1,5 @@
+# Authour: 	azmathias Stuperuser Ltd
+# Purpose: Pythagorean Triples Checker
+# 		Allows the user to input the sides of any triangle in any order.
+# 		Return whether the triangle is a Pythagorean Triple or not.
+# 		Loop the program so the user can use it more than once without having to restart the program.
