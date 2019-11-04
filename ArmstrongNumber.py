f079ad2183d5b76f41b5d4c24cbf2108859457dc
azmathias <azramathias@stuperuser.onmicrosoft.com>
2019-11-04 21:45:20 +0000

    Add Error Handling

--- ArmstrongNumber.py
+++ ArmstrongNumber.py
@@ -1,46 +1,64 @@
 # Authour: 	azmathias Stuperuser Ltd
 # Purpose:	Define a function that allows the user to check whether a given number is armstrong number or not.
 #			Number x, no of digits = n, if the sum of each digit raised to the power of n is equal to x, the number is an armstrong number (also called narcisstic numbers)		
-# Notes:	Examples of Armstrong Numbers are: 153, 407, 1634, 
-# TODO:		Add error handling if user does not enter a number.
+# Notes:	Examples of Armstrong Numbers are: 153, 407, 1634 
 
-testing_active = True
-
-print("Armstrong Number Checker \n")
-
-try:
-	while testing_active:
-		num = input("Please enter a number:")
-		num = int(num)
-			
-		# calculate the length of the input number
-			pownum = len(str(num))
-			
-		sum = 0
-			
-		# determine Armstrong number status
-		resnum = num
-		while resnum > 0:
-			digit = resnum % 10
-			sum += digit ** pownum
-			resnum //= 10
-		
-		# return results
-		if num == sum:
-			print(num,"is an Armstrong number")
-		else:
-			print(num,"is not an Armstrong number")
-			
-		# test again? 
-		repeat = input("\nWould you like to test another number? (y/n) ")
-		if repeat == 'n':
-			testing_active = False
-
-		break
-		
+# get number to test
+def inputNum(message):
+	while True:
+		try:
+			userinput = int(input(message))
 		except ValueError:
-			print("Please enter a number: ")
+			print("\nPlease input a valid number:")
+			continue
+		else:
+			return userinput
+			break
 
+# determine Armstrong number status			
+def armstrongNumcheck(number):
+	pownum = len(str(number))
+	sum = 0
+	resnum = number
+	while resnum > 0:
+		digit = resnum % 10
+		sum += digit ** pownum
+		resnum //= 10
+	# return results
+	if number == sum:
+		print(number,"is an Armstrong number")
+	else:
+		print(number,"is not an Armstrong number")
+		
+# allow user to test again		
+def testStatus():
+	testQ = "\nType q to quit, or hit <Enter> to test another number."
+	print(testQ)
+	new_value = 0
+	while not new_value:
+		response = input('> ')
+		if response == '':
+			print()
+		elif response == 'q':
+			new_value = -1
+		else:
+			print("\nPlease try again.")
+			print(testQ)
+			continue
+		return new_value
+
+# program starts here
+def main():
+	print("Welcome to the Armstrong Number Checker \n")   
+	value = 0
+	while value != -1:
+		num = inputNum("Please enter a number:")
+		armstrongNumcheck(num)
+		value = testStatus()
+	print("\nThank you for playing!")
+	
+
+if __name__ == '__main__': main()
 		
 
 
