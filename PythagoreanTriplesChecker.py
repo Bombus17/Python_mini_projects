e9844ef408a21c3e59cf5d105cbd900e3f413419
azmathias <azramathias@stuperuser.onmicrosoft.com>
2019-11-05 13:15:37 +0000

    Add Pythagorean Triples

--- PythagoreanTriplesChecker.py
+++ PythagoreanTriplesChecker.py
@@ -6,11 +6,11 @@
 
 import math
 
-def pythagorean_triplet(n):
+def pythagorean_triplet(a, b, c):
 	for b in range(n):
 		for a in range(1, b):
 			c = math.sqrt(a * a + b * b)
 			if c % 1 == 0:
 				print(a, b, int(c))
 				
-pythagorean_triplet(12)			
+pythagorean_triplet(1,2,3)			
